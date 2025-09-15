c48ff3f049e12292574abbc6fa9716b485b9bc45
Ashi102506 <agolyan_be24@thapar.edu>
2025-09-15 23:22:06 +0530

    q3_as5.cpp

--- q3_as5.cpp
+++ q3_as5.cpp
@@ -1,62 +1,52 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+#include<iostream>
+using namespace std;
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+struct Node {
+    int data;
+    Node* next;
+};
 
-    def insert(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
+void insert(Node*& head, int x) {
+    Node* n = new Node();
+    n->data = x;
+    n->next = NULL;
+    
+    if (head == NULL) {
+        head = n;
+        return;
+    }
+    
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
 
-    def display(self):
-        result = []
-        current = self.head
-        while current:
-            result.append(current.data)
-            current = current.next
-        return result
+int findMiddle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+    
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    return slow->data;
+}
 
-    def find_middle(self):
-        if not self.head:
-            return None
-
-        slow = self.head
-        fast = self.head
-
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-
-        return slow.data
-
-def main():
-    ll = LinkedList()
-
-    values = [1, 2, 3, 4, 5]
-    for val in values:
-        ll.insert(val)
-
-    print("Linked List:", ll.display())
-    middle = ll.find_middle()
-    print("Middle element:", middle)
-
-    ll2 = LinkedList()
-    values2 = [1, 2, 3, 4]
-    for val in values2:
-        ll2.insert(val)
-
-    print("\nLinked List:", ll2.display())
-    middle2 = ll2.find_middle()
-    print("Middle element:", middle2)
-
-if __name__ == "__main__":
-    main()
+int main() {
+    Node* head = NULL;
+    int n, x;
+    
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        insert(head, x);
+    }
+    
+    int middle = findMiddle(head);
+    cout << middle << endl;
+    
+    return 0;
+}
